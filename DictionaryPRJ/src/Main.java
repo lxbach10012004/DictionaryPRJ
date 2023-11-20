bf66ce398506f609f995d75bbbf385cd7c6f3cda
lxbach10012004 <22024506@vnu.edu.vn>
2023-11-20 11:32:03 +0700

    Added new database
    - Added new database with more than 500.000 words
    - Fixed file directory
    - Fixed some bugs

--- DictionaryPRJ/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Dictionary dictionary = new Dictionary();
-        DictionaryCommandLine.dictionaryAdvanced(dictionary);
-    }
-}
